fbe4ae10a60277a4b7a53daa436a931605aed57f
Aishwarya-Chilshetty <chilshetty77@gmail.com>
2025-10-04 11:36:11 +0530

    Setup the LLM

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
+import { ChatGroq } from "@langchain/groq";
+
 console.log("Welcome to the personal assistant!");
+
+
+const tools : any[] = [];
+
+//add GROQ_API_KEY in env file
+const model = new ChatGroq({
+    model: "openai/gpt-oss-120b",
+    temperature: 0
+}).bindTools(tools);
