1ee20a9b935354b6ea00f32b698632986d380c4a
Aishwarya-Chilshetty <chilshetty77@gmail.com>
2025-10-04 12:24:12 +0530

    Setup the tools

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import { ChatGroq } from "@langchain/groq";
+import { createEventTool, getEventTool } from "./tools";
 
-console.log("Welcome to the personal assistant!");
-
-
-const tools : any[] = [];
+const tools : any[] = [createEventTool, getEventTool];
 
 //add GROQ_API_KEY in env file
 const model = new ChatGroq({
     model: "openai/gpt-oss-120b",
     temperature: 0
 }).bindTools(tools);
+
---
+++ tools.ts
@@ -0,0 +1,33 @@
+import { tool } from '@langchain/core/tools';
+import { z } from 'zod';
+
+export const createEventTool = tool(
+    async () => {
+        // Google calendar logic goes
+        return 'Meeting has been created';
+    },
+    {
+        name: 'create-event',
+        description: 'Call to create a calendar event.',
+        schema: z.object({})
+    }
+);
+
+export const getEventTool = tool(
+    async () => {
+        // Google calendar logic goes
+        return JSON.stringify([
+            {
+                title: 'Meeting with Sujoy',
+                date: '9th Aug 2025',
+                time: '2 PM',
+                location: 'Gmeet',
+            },
+        ]);
+    },
+    {
+        name: 'get-events',
+        description: 'Call to get the calendar events.',
+        schema: z.object({})
+    }
+);
